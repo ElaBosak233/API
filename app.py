23679fb5cfee4718f60877e354764da6bb1690b6
埃拉拉 <ElaBosak233@qq.com>
2022-04-16 10:57:12 +0800

    update

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 # FastAPI 对象
 app: FastAPI = FastAPI()
+pages: Jinja2Templates = Jinja2Templates(directory="__pages__")
 
 
 # 创建 Serverless 函数
@@ -14,8 +16,8 @@ async def add_no_cache_header(request: Request, call_next):
 
 # 首页
 @app.get("/")
-async def index():
-    return {"message": "Hello World"}
+async def index(request: Request):
+    return pages.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/hello/{name}")
